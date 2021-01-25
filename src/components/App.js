6b82bba487051ca821b044c69cd0c31fdefaa639
praz <t.prazwal@gmail.com>
2021-01-25 16:43:50 +0100

    add routing to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
-import DrinkList from '../containers/DrinkList';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Catalogue from '../containers/Catalogue';
+import DrinkDetails from '../components/DrinkDetails';
 import BrowseIndex from './BrowseIndex';
 
 const App = () => (
-  <div className="App">
+  <>
     <BrowseIndex />
-    <DrinkList />
-  </div>
+    <Route exact path="/">
+      <Catalogue />
+    </Route>
+    <Route exact path="/:id">
+      <DrinkDetails />
+    </Route>
+    <Footer />
+  </>
 );
 
 export default App;
