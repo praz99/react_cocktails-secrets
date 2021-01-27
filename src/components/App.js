bb00bb0875accbce7b2ef137edd8f6bdc2493d23
praz <t.prazwal@gmail.com>
2021-01-27 16:00:50 +0100

    add and style the navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import BrowseIndex from './BrowseIndex';
 import Footer from '../layouts/Footer';
+import Navbar from '../layouts/Navbar';
 
 const App = () => (
   <>
+    <Navbar />
     <BrowseIndex />
     <Footer />
   </>
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import selectCategories from '../constants/categories';
+import '../styles/CategoryFilter.css';
 
 const CategoryFilter = ({ handleFilterChange }) => (
-  <select onChange={handleFilterChange} name="category">
+  <select onChange={handleFilterChange} name="category" className="category-select">
     <option value="All">All</option>
     {selectCategories}
   </select>
