ec498aec9f56e9e200fd3e78d79c6f65f0426aa0
praz <t.prazwal@gmail.com>
2021-01-28 18:36:07 +0100

    add test for BrowseIndex component

---
+++ src/__tests__/components/BrowserIndex.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ShallowRenderer from 'react-test-renderer/shallow';
+import BrowseIndex from '../../components/BrowseIndex';
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useLocation: () => ({
+    pathname: "http://localhost:3000/search?f=d"
+  })
+}));
+
+describe('BrowseIndex component', () => {
+  it('Renders correctly', () => {
+    const renderer = new ShallowRenderer();
+    const wrapper = renderer.render(<BrowseIndex />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
