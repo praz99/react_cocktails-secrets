342d417c0c0520b143bab1b9ec13c91ab6c63138
praz <t.prazwal@gmail.com>
2026-05-23 12:24:36 +0545

    refactor tests to use MemoryRouter for routing context in BrowseIndex, Drink, and Navbar components

--- src/__tests__/components/BrowserIndex.test.js
+++ src/__tests__/components/BrowserIndex.test.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import ShallowRenderer from 'react-test-renderer/shallow';
-import BrowseIndex from '../../components/BrowseIndex';
+import React from "react";
+import ShallowRenderer from "react-test-renderer/shallow";
+import BrowseIndex from "../../components/BrowseIndex";
+import { vi } from "vitest";
 
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useLocation: () => ({
-    pathname: 'http://localhost:3000/search?f=d',
-  }),
-}));
+vi.mock("react-router-dom", async () => {
+  const actual = await vi.importActual("react-router-dom");
+  return {
+    ...actual,
+    useLocation: () => ({ pathname: "http://localhost:3000/search?f=d" }),
+  };
+});
 
-describe('BrowseIndex component', () => {
-  it('Renders correctly', () => {
+describe("BrowseIndex component", () => {
+  it("Renders correctly", () => {
     const renderer = new ShallowRenderer();
     const wrapper = renderer.render(<BrowseIndex />);
     expect(wrapper).toMatchSnapshot();
--- src/__tests__/layouts/Navbar.test.js
+++ src/__tests__/layouts/Navbar.test.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import { render, screen, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Navbar from '../../layouts/Navbar';
+import React from "react";
+import renderer from "react-test-renderer";
+import { render, screen, cleanup } from "@testing-library/react";
+import Navbar from "../../layouts/Navbar";
+import { MemoryRouter } from "react-router-dom";
 
 afterEach(cleanup);
 
-it('renders Navbar component correctly', () => {
-  const nav = renderer.create(<Navbar />).toJSON();
+it("renders Navbar component correctly", () => {
+  const nav = renderer
+    .create(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>,
+    )
+    .toJSON();
   expect(nav).toMatchSnapshot();
 });
 
-it('should display the heading', () => {
-  const { getByTestId } = render(<Navbar />);
-  expect(getByTestId('navbar-heading')).toHaveTextContent('The CockTails');
+it("should display the heading", () => {
+  const { getByTestId } = render(
+    <MemoryRouter>
+      <Navbar />
+    </MemoryRouter>,
+  );
+  expect(getByTestId("navbar-heading")).toHaveTextContent("The CockTails");
 });
 
-it('should display link for home page', () => {
-  render(<Navbar />);
-  const homeAnchorNode = screen.getByText('The CockTails');
+it("should display link for home page", () => {
+  render(
+    <MemoryRouter>
+      <Navbar />
+    </MemoryRouter>,
+  );
+  const homeAnchorNode = screen.getByText("The CockTails");
   expect(homeAnchorNode).toBeInTheDocument();
 });
